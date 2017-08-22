751fd8c66b9c6d51133f3eaa7b2b92092c6e688b
张佳迪 <309357963@qq.com>
2017-08-22 11:35:54 +0800

    commit

--- src/android/SplashScreen.java
+++ src/android/SplashScreen.java
@@ -296,7 +296,7 @@ public class SplashScreen extends CordovaPlugin {
                 Display display = cordova.getActivity().getWindowManager().getDefaultDisplay();
                 Context context = webView.getContext();
 
-                cordova.getActivity().getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+                //cordova.getActivity().getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
 
                 // Use an ImageView to render the image because of its flexible scaling options.
                 viewsss = new LottieAnimationView(context);
@@ -314,24 +314,6 @@ public class SplashScreen extends CordovaPlugin {
                 viewsss.playAnimation();
 
                 splashDialog = new Dialog(context, android.R.style.Theme_NoTitleBar_Fullscreen);
-                splashDialog.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-                splashDialog.getWindow().getDecorView().setOnSystemUiVisibilityChangeListener(new OnSystemUiVisibilityChangeListener() {
-                    @Override
-                    public void onSystemUiVisibilityChange(int visibility) {
-                        int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
-                                //布局位于状态栏下方
-                                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
-                                //全屏
-                                //隐藏导航栏
-                                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION ;
-                        if (Build.VERSION.SDK_INT >= 19) {
-                            uiOptions |= 0x00001000;
-                        } else {
-                            uiOptions |= View.SYSTEM_UI_FLAG_LOW_PROFILE;
-                        }
-                        splashDialog.getWindow().getDecorView().setSystemUiVisibility(uiOptions);
-                    }
-                });
                 splashDialog.setContentView(viewsss);
                 splashDialog.setCancelable(false);
                 splashDialog.show();
